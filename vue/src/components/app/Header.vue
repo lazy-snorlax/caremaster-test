d4d91bf36a2809c1ccdbf74feec1ed8b3f0ed01c
Anthony <blacknebula236@gmail.com>
2025-05-27 17:01:08 +0930

    change sidebar icon into 3 bars

--- vue/src/components/app/Header.vue
+++ vue/src/components/app/Header.vue
@@ -1,7 +1,9 @@
 <template>
     <header class="page-header" v-if="title != null && title != ''">
         <div class="left">
-            <button id="menu-toggle" class="btn" @click="sidebarToggle">X</button>
+            <a id="" class="btn btn-square btn-ghost" @click="sidebarToggle">
+                <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current md:h-6 md:w-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            </a>
         </div>
         <div class="middle">
             <div class="page-title mx-3 my-3 text-center">
