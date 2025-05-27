a2d9c63c17155a2e2d687a58454cd018e8445e4a
Anthony <blacknebula236@gmail.com>
2025-05-27 13:32:35 +0930

    correct employee props + adjusting css & markup

--- vue/src/views/admin/Company.vue
+++ vue/src/views/admin/Company.vue
@@ -34,11 +34,11 @@
             </div>
         </div>
 
-        <div class="flex">
-            <h2 class="text-3xl text-left text-green-500 mt-10">EMPLOYEES</h2>
+        <div class="flex mt-10">
+            <h2 class="text-3xl text-left text-green-500">EMPLOYEES</h2>
             <button class="mt-auto ms-auto btn btn-success">+ Employee</button>
         </div>
-        <div class="card bg-gray-700 text-green-500 card-md shadow-sm mb-2">
+        <div class="card bg-gray-700 text-green-500 card-md shadow-sm mt-3 mb-2">
             <div class="card-body grid grid-cols-5 text-green-500 border-b-2">
                 <div class="card-title">FIRST</div>
                 <div class="card-title">LAST</div>
@@ -51,18 +51,18 @@
                     <div class="text-2xl text-center">No employees found</div>
                 </div>
             </template>
-            <div v-else class="card-body grid grid-cols-5 listitem">
-                <template v-for="employee in company?.employees">
-                    <div class="card-title">{{ employee?.name }}</div>
-                    <div class="card-title">{{ employee?.name }}</div>
-                    <div class="card-title">{{ employee?.name }}</div>
-                    <div class="card-title">{{ employee?.name }}</div>
+            <template v-else>
+                <div v-for="employee in company?.employees" class="card-body py-3 grid grid-cols-5 listitem">
+                    <div class="card-title">{{ employee?.first_name }}</div>
+                    <div class="card-title">{{ employee?.last_name }}</div>
+                    <div class="card-title">{{ employee?.email }}</div>
+                    <div class="card-title">{{ employee?.address }}</div>
                     <div class="card-title">
                         <button class="btn btn-soft btn-success">Edit</button>
                         <button class="btn btn-outline btn-error">Delete</button>
                     </div>
-                </template>
-            </div>
+                </div>
+            </template>
         </div>
     </Container>
 </template>
