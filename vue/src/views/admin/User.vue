4f0ef4685db7f90add4ac3a38fe5cd33c6923004
Anthony <blacknebula236@gmail.com>
2025-06-13 15:21:33 +0930

    add role & status

--- vue/src/views/admin/User.vue
+++ vue/src/views/admin/User.vue
@@ -16,6 +16,28 @@
                     </label>
                     <input type="text" class="input w-full rounded-md border border-gray-600" v-model="userEmail" />
                 </div>
+                <div class="flex gap-5">
+                    <div class="w-1/2">
+                        <label class="label">
+                            <span class="text-green-500 label-text font-bold">Role</span>
+                        </label>
+                        <select class="select w-full" v-model="selectedRole">
+                            <option disabled value="">Please select one</option>
+                            <option>User</option>
+                            <option>Admin</option>
+                        </select>
+                    </div>
+                    <div class="w-1/2">
+                        <label class="label">
+                            <span class="text-green-500 label-text font-bold">Status</span>
+                        </label>
+                        <select class="select w-full" v-model="selectedStatus">
+                            <option disabled value="">Please select one</option>
+                            <option>ENABLED</option>
+                            <option>DISABLED</option>
+                        </select>
+                    </div>
+                </div>
             </div>
             <div class="text-center mb-2">
                 <button class="btn btn-success" @click="submit">Update User Details</button>
@@ -27,7 +49,7 @@
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia';
 import { useRoute, useRouter } from 'vue-router';
-import { onBeforeMount } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import { useUserStore } from '../../stores/users';
 import { getUserComputedVals } from '../../composables/user-computed';
 
@@ -38,18 +60,28 @@ const { getUser, updateUser } = useUserStore()
 const {
     userId,
     userName,
-    userEmail
- } = getUserComputedVals()
+    userEmail,
+    userRole,
+    userStatus,
+} = getUserComputedVals()
+const selectedRole = ref(userRole)
+const selectedStatus = ref(userStatus)
 
 onBeforeMount(async () => { await getUser(route.params.id) })
 const submit = async () => {
     try {
+        if (!userRole.value) {
+            console.log(">>> UserRole: ", userRole.value)
+            return
+        }
         const payload = {
             id: userId.value,
             name: userName.value,
             email: userEmail.value,
+            role: userRole.value,
         }
         console.log(">>> Update Payload: ", payload)
+        updateUser(payload)
     } catch (error) {
         console.log(">>>> Errors: ", error)
     }
